4c844275acb05fdc59b13c94df30059067539a9f
butterbeetle <butterbeetle@naver.com>
2024-11-09 21:17:19 +0900

    [init] router 세팅

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,6 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "../components/common/Layout";
 import HomePage from "../pages/HomePage/HomePage";
+import LoginPage from "../pages/LoginPage/LoginPage";
+import MyPage from "../pages/MyPage/MyPage";
+import SignUpPage from "../pages/SignUpPage/SignUpPage";
 
 const router = createBrowserRouter([
   {
@@ -9,6 +12,18 @@ const router = createBrowserRouter([
       {
         path: "/",
         element: <HomePage />,
+      },
+      {
+        path: "/my",
+        element: <MyPage />,
+      },
+      {
+        path: "/signup",
+        element: <SignUpPage />,
+      },
+      {
+        path: "/login",
+        element: <LoginPage />,
       },
     ],
   },
